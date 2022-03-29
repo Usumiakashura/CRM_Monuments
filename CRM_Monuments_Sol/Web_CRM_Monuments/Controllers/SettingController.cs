c55e9b204b1c309654c59caeec1c1a611a1eaadb
Aliaksandra Manulik <aliaksandra.manulik@gmail.com>
2022-03-29 17:45:40 +0300

    Администратор может добавлять пользователей

--- CRM_Monuments_Sol/Web_CRM_Monuments/Controllers/SettingController.cs
+++ CRM_Monuments_Sol/Web_CRM_Monuments/Controllers/SettingController.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_CRM_Monuments.Models;
 
 namespace Web_CRM_Monuments.Controllers
 {
@@ -18,7 +20,6 @@ namespace Web_CRM_Monuments.Controllers
         private DataManager _dataManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
-
 
         public SettingController(DataManager dataManager, 
             UserManager<ApplicationUser> userManager, 
@@ -34,17 +35,104 @@ namespace Web_CRM_Monuments.Controllers
         [Authorize(Roles = "admin")]
         public ActionResult Settings()
         {
-            
-
             return View();
         }
 
+        //--------- Настройки пользователей --------------
         [Authorize(Roles = "admin")]
-        public async Task<ActionResult> AllUsers()
+        public async Task<ActionResult> AllUsers() => View(await _dataManager.ApplicationUsersRepository.GetAllUsers());
+        
+        [Authorize(Roles = "admin")]
+        public IActionResult CreateUser() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserViewModel model)
         {
-            return View(await _dataManager.ApplicationUsersRepository.GetAllUsers());
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                user = await _userManager.FindByEmailAsync(model.Email);
+                await _userManager.AddToRoleAsync(user, model.Role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return RedirectToAction("AllUsers", "Setting");
         }
 
+        public async Task<IActionResult> EditUser(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            UserViewModel model = new UserViewModel { Id = user.Id, Email = user.Email, Name = user.Name };
+            foreach (var role in await _userManager.GetRolesAsync(user))
+            {
+                model.Role = role;
+            }
+            return RedirectToAction("EditUser", model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.Name = model.Name;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    user = await _userManager.FindByEmailAsync(model.Email);
+                    await _userManager.RemoveFromRolesAsync(user, await _userManager.GetRolesAsync(user));
+                    await _userManager.AddToRoleAsync(user, model.Role);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteUser(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        //--------------------------------------------------
+        //--------- Настройки типов портретов --------------
         [Authorize(Roles = "admin")]
         public ActionResult AllTypePortraits()
         {
