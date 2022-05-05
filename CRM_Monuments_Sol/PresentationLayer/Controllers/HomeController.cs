84a85b8a68240c090fbbf23d937ec4d17374159d
Aliaksandra Manulik <aliaksandra.manulik@gmail.com>
2022-05-05 17:44:36 +0300

    Добавила PresentationLayer, который будет работать с Vue.js

---
+++ CRM_Monuments_Sol/PresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using BuissnesLayer.Services;
+using DataLayer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment _hostingEnvironment;
+        private ServicesManager _servicesManager;
+
+        public HomeController(ILogger<HomeController> logger, DataManager dataManager, IWebHostEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _servicesManager = new ServicesManager(dataManager);
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
